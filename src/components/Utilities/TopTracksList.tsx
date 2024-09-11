870dd395593a03a4fff2454ed4a5e4d4dc104d01
Saoirselicious <seebersaoirse@gmail.com>
2024-09-11 17:44:42 +0100

    Pointing to backend server

--- src/components/Utilities/TopTracksList.tsx
+++ src/components/Utilities/TopTracksList.tsx
@@ -17,7 +17,7 @@ interface Props {
 }
 
 const fetchWebApi = async (endpoint: string, token: string) => {
-    const url = `http://localhost:8000${endpoint}`;
+    const url = `http://profitable-sheri-seebers-8755823d.koyeb.app/${endpoint}`;
     console.log(endpoint);
     console.log(token);
     console.log(`Request URL: ${url}`);
@@ -66,7 +66,7 @@ const TopTracksList: React.FC<Props> = ({ token }) => {
         if (tracks.length > 0) {
             const sendTracksData = async () => {
                 try {
-                    const response = await axios.post('http://localhost:8000/receive-tracks', tracks, {
+                    const response = await axios.post('http://profitable-sheri-seebers-8755823d.koyeb.app//receive-tracks', tracks, {
                         headers: { 'Content-Type': 'application/json' },
                     });
                     console.log('Tracks data sent successfully:', response.data);
