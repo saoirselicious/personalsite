8428f51ce1d52caf8511dc804af640fee26c954b
Saoirselicious <seebersaoirse@gmail.com>
2024-09-16 15:51:47 +0100

    Trying browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Homepage from './components/Homepage/Homepage';
 import Navbar from './components/Navbar/Navbar';
-import Skills from "./components/Homepage/Skills"
-import Expierence from "./components/Homepage/Timeline"
+import Skills from "./components/Homepage/Skills";
+import Experience from "./components/Homepage/Timeline";
 import Portfolio from './components/Homepage/Portfolio';
 import CV from './components/CV/CV';
 import TopTracks from './components/Utilities/TopTracks';
 import TopTracksList from './components/Utilities/TopTracksList';
 import SpotifyCallback from './components/Utilities/SpotifyCallback';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LoadingProvider, LoadingSplash, useLoading, ErrorProvider, ErrorSplash, useError, NotFoundSplash } from './components/Splashscreen/SplashScreen';
 
 export default function App() {
-  return <LoadingProvider><ErrorProvider><AppContent /></ErrorProvider></LoadingProvider>
+  return (
+    <LoadingProvider>
+      <ErrorProvider>
+        <AppContent />
+      </ErrorProvider>
+    </LoadingProvider>
+  );
 }
 
 function AppContent() {
@@ -43,11 +49,10 @@ function AppContent() {
   }, []);
 
   if (error) {
-    <Navbar toggleTheme={toggleTheme} />
     return <ErrorSplash />;
   }
+
   if (loading) {
-    <Navbar toggleTheme={toggleTheme} />
     return <LoadingSplash />;
   }
 
@@ -57,7 +62,7 @@ function AppContent() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/experience" element={<Expierence />} />
+        <Route path="/experience" element={<Experience />} />
         <Route path="/resume" element={<CV />} />
         <Route path="/projects" element={<Portfolio />} />
         <Route path="/projects/sortihue" element={<TopTracks />} />
