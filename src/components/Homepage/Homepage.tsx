697b15bf256ea309ccbc81f211e7a5baea684111
Saoirselicious <seebersaoirse@gmail.com>
2024-09-18 16:46:03 +0100

    removing unused usestate

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Box, Typography, Button, IconButton } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
