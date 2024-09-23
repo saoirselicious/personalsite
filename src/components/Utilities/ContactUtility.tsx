570d1ef98eea1a88bf683d07f333ddd69520ca4d
Saoirselicious <seebersaoirse@gmail.com>
2024-09-23 11:53:56 +0100

    commenting out unused variable for the minute

--- src/components/Utilities/ContactUtility.tsx
+++ src/components/Utilities/ContactUtility.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+// const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export async function fetchRecaptchaConfig() {
     try {
